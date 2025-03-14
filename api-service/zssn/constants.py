e451dad72cff2796f6084e18bd2830d1ddf4c6e4
Leonid Yeromin <leon.yeromin@gmail.com>
2025-03-14 15:02:53 +0100

    add: models for Survivor, Item, Inventory. Migrations

---
+++ api-service/zssn/constants.py
@@ -0,0 +1,7 @@
+GENDER = [
+    ("male", "Male"),
+    ("female", "Female"),
+    ("other", "Other"),
+]
+
+INFECTED_REPORT_THRESHOLD = 3
---
+++ api-service/zssn/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from datetime import date
+from .constants import GENDER
+
+
+class Survivor(models.Model):
+    name = models.CharField(max_length=100)
+    age_on_registration = models.PositiveIntegerField()
+    registration_year = models.PositiveIntegerField(default=date.today().year)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    is_infected = models.BooleanField(default=False)
+    infected_reported_by = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        blank=True,
+        related_name="infected_survivors_reported",
+    )
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    points = models.PositiveIntegerField()
+
+
+class Inventory(models.Model):
+    survivor = models.ForeignKey(
+        Survivor, on_delete=models.CASCADE, related_name="inventory"
+    )
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
